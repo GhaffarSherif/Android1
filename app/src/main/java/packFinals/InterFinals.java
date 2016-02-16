899e399c567c47d00e7cadb5d1e7c6cd2d1407a0
Lucas Kourouklis <lucshadow13@hotmail.com>
2016-02-16 17:07:04 -0500

    No time to comment.

--- app/src/main/java/packFinals/InterFinals.java
+++ app/src/main/java/packFinals/InterFinals.java
@@ -6,15 +6,9 @@ import java.util.*;
  * Created by Shadowfire2303 on 15/02/2016.
  */
 public interface InterFinals {
-    public ArrayList<String> id = new ArrayList<String>();
-    public ArrayList<String> name = new ArrayList<String>();
-    public ArrayList<Double> grade1 = new ArrayList<Double>();
-    public ArrayList<Double> grade2 = new ArrayList<Double>();
-    public ArrayList<Double> grade3 = new ArrayList<Double>();
-    public ArrayList<Double> finalGrade = new ArrayList<Double>();
-    public ArrayList<String> letterGrade = new ArrayList<String>();
-    public ArrayList<Integer> isProgramming1 = new ArrayList<Integer>();
-
-    public int calculateFinalGrade();
-    public void displayAllGrades();
+    public final char GRADE_A = 'A';
+    public final char GRADE_B = 'B';
+    public final char GRADE_C = 'C';
+    public final char GRADE_D = 'D';
+    public final char GRADE_F = 'F';
 }
--- app/src/main/java/packFinals/clsFinals.java
+++ app/src/main/java/packFinals/clsFinals.java
@@ -1,7 +1,21 @@
 package packFinals;
 
+import java.util.*;
+
 /**
  * Created by Shadowfire2303 on 15/02/2016.
  */
 public class clsFinals {
+    public ArrayList<String> id = new ArrayList<String>();
+    public ArrayList<String> name = new ArrayList<String>();
+    public ArrayList<Double> grade1 = new ArrayList<Double>();
+    public ArrayList<Double> grade2 = new ArrayList<Double>();
+    public ArrayList<Double> grade3 = new ArrayList<Double>();
+    public ArrayList<Double> finalGrade = new ArrayList<Double>();
+    public ArrayList<String> letterGrade = new ArrayList<String>();
+    public ArrayList<Integer> isProgramming1 = new ArrayList<Integer>();
+
+    public static calculate(){
+
+    }
 }
